9ddddf268f7d548d627a6353e4da3358acd6ade2
PabloFdez <pablofernandez.fi@gmail.com>
2018-12-04 16:17:34 +0100

    Initial commit

---
+++ src/main/java/es/upm/grise/profundizacion2018/ProfPabloFdez_Maven/App.java
@@ -0,0 +1,13 @@
+package es.upm.grise.profundizacion2018.ProfPabloFdez_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
